3e3c1424f53207edc65be59c7b1c951f6434eec4
CarlosJChileS <carloschilesilva@gmail.com>
2025-04-29 21:37:24 -0500

    feat: initialize TypeScript project with package.json, app.ts, and tsconfig.json
    - Added package.json with scripts for development, build, and start.
    - Created app.ts with student management functionality including adding students and handling asynchronous operations.
    - Configured tsconfig.json for TypeScript compilation settings, enabling strict type checking and module interop.

---
+++ 1parcial/semana 2/src/app.ts
@@ -0,0 +1,111 @@
+import { rejects } from "node:assert";
+import { resolve } from "node:path";
+
+// Definir variables
+//let greeting: string = "hello";
+//const name2: number = 123;
+//console.log(greeting);
+
+// Definir un objeto
+const student: IStudent = {
+    id: 1,
+    name: 'John',
+    email: 'john@gmail.com',
+    address: 'his house',
+};
+
+interface IStudent {
+    id: number;
+    name: string;
+    email: string;
+    address: string;
+    grade?: number; // Propiedad opcional con ?
+}
+
+// Definir arreglo
+const students: IStudent[] = [
+    {
+        id: 123,
+        name: 'Carlos',
+        email: 'carlos@gmail.com',
+        address: 'his house',
+    },
+    {
+        id: 3,
+        name: 'Junior',
+        email: 'junior@gmail.com',
+        address: 'junior\'s place'
+    },
+];
+
+// Agregar elemento después de crear el arreglo
+students.push({
+    id: 124, 
+    name: 'Walter', 
+    email: "walter@gmail.com", 
+    address: 'bathroom'
+});
+
+// Función para agregar estudiantes
+function addStudent(studentList: IStudent[], newStudent: IStudent): void {
+    studentList.push(newStudent);
+}
+
+// Ejemplo de uso de la función addStudent
+const newStudent: IStudent = {id: 12345,  name: '', email: '',address: '' };
+
+addStudent(students, newStudent);
+
+
+//callback funcion para el ultimo parametro , 
+
+function  addStudent2(parm:IStudent, callback: (student: IStudent) => void): void {
+    students.push(parm);
+    callback(parm);
+}
+
+// Ejemplo de uso de la función addStudent2
+const studentA2: IStudent = {id: 12345,  name: '', email: '',address: '' };
+
+// Llamar a la función addStudent2 y pasar una función de callback
+addStudent2(studentA2, (parm: IStudent) => console.log);
+
+
+// Ejemplo de uso de la función addStudent2 para agregar un nuevo estudiante y mostrarlo en consola
+function addStudent3(parm: IStudent): Promise<IStudent> {
+    return new Promise((resolve) => {
+        students.push(parm);
+        setTimeout(() => {
+            resolve(parm);
+        }, 10000);
+    });
+    // podemos agregar un reject para manejar errores
+}
+
+// Llamar a la función addStudent3 y manejar la promesa
+addStudent3 (student).then((student) => {
+    console.log(student);
+})
+
+
+
+// await es una forma de esperar a que una promesa se resuelva antes de continuar con la ejecución del código.
+// ultimas vesiones da error
+
+// await ser invocado dentro de una funcion asincrona async para que funcione correctamente
+async function main() {
+    await addStudent3(student);
+    try
+    {
+        await addStudent3(student);
+    }
+    catch(ex){
+
+    }
+    finally{
+
+    }
+    console.log(student);
+}
+
+main(); // Llamar a la función main para ejecutar el código asincrono
---
+++ 1parcial/typescript/src/app.ts
@@ -0,0 +1,6 @@
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+const resultado = sumar(5, 10);
+console.log("El resultado es:", resultado);
